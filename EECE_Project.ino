7babd2426930b37d3d713fea18d576868f50f722
asolis3 <116390105+asolis3@users.noreply.github.com>
2022-10-21 15:36:53 -0500

    Add files via upload

---
+++ EECE_Project.ino
@@ -0,0 +1,145 @@
+int buzzer = 12; //buzzer is in pin 11
+
+void setup() 
+{
+    pinMode(buzzer,OUTPUT); //make the buzzer work
+}
+
+void loop() {
+  
+ 
+  tone(buzzer, 466); //Bb
+  delay(250);
+  noTone(buzzer);
+
+   tone(buzzer, 466);
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 466);
+  delay(450);
+  noTone(buzzer);
+
+  tone(buzzer, 440); //A
+  delay(250);
+  noTone(buzzer);
+
+   tone(buzzer, 440);
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 440);
+  delay(450);
+  noTone(buzzer);
+
+  tone(buzzer, 392); //G
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 440); //A
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 392); //G
+  delay(450);
+  noTone(buzzer);
+
+  tone(buzzer, 293); //D
+  delay(850);
+  noTone(buzzer);
+
+  tone(buzzer, 392); //G
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 440); //A
+  delay(250);
+  noTone(buzzer);
+  
+  tone(buzzer, 392); //G
+  delay(450);
+  noTone(buzzer);
+  
+  tone(buzzer, 293); //D
+  delay(450);
+  noTone(buzzer);
+  
+  tone(buzzer, 349); //F
+  delay(450);
+  noTone(buzzer);
+
+   tone(buzzer, 392); //G
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 440); //A
+  delay(250);
+  noTone(buzzer);
+
+   tone(buzzer, 392); //G
+  delay(450);
+  noTone(buzzer);
+
+  tone(buzzer, 311); //Eb
+  delay(850);
+  noTone(buzzer);
+
+   tone(buzzer, 261); //C
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 293); //D
+  delay(450);
+  noTone(buzzer); 
+  
+  tone(buzzer, 311); //Eb
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 349); //F
+  delay(250);
+  noTone(buzzer);
+
+   tone(buzzer, 392); //G
+  delay(450);
+  noTone(buzzer);
+
+  tone(buzzer, 349); //F
+  delay(250);
+  noTone(buzzer); 
+  
+  tone(buzzer, 261); //C
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 440); //D
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 311); //Eb
+  delay(450);
+  noTone(buzzer);
+
+   tone(buzzer, 349); //F
+  delay(850);
+  noTone(buzzer);
+
+  tone(buzzer, 0); // Rest
+  delay(450);
+  noTone(buzzer); 
+  
+  tone(buzzer, 392); //G
+  delay(250);
+  noTone(buzzer);
+
+  tone(buzzer, 369); //F#
+  delay(250);
+  noTone(buzzer);
+
+  
+  
+ 
+  delay(10000000000);
+  
+}
+   
